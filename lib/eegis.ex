106eb1dba7fb13a3c00576cb127f7815f1f7c9a1
Andrea Lai <alai.arpas@gmail.com>
2023-06-22 12:44:33 +0200

    first commit

---
+++ lib/eegis.ex
@@ -0,0 +1,18 @@
+defmodule Eegis do
+  @moduledoc """
+  Documentation for `Eegis`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Eegis.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/eegis_test.exs
@@ -0,0 +1,8 @@
+defmodule EegisTest do
+  use ExUnit.Case
+  doctest Eegis
+
+  test "greets the world" do
+    assert Eegis.hello() == :world
+  end
+end
